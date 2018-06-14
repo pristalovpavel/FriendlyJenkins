933d6ecb9a1de9ffdffd8b3bc1383203cb5de5b2
Pavel Pristalov <pristalovpavel@gmail.com>
2018-06-14 19:44:45 +0300

    other retrofit tunes

--- app/src/main/java/ru/pristalovpavel/personal/friendlyjenkins/model/JobListElement.kt
+++ app/src/main/java/ru/pristalovpavel/personal/friendlyjenkins/model/JobListElement.kt
@@ -3,8 +3,8 @@ package ru.pristalovpavel.personal.friendlyjenkins.model
 import com.google.gson.annotations.SerializedName
 
 data class JobListElement(
-        @SerializedName("_class") val jenkinsClassName: String,
-        val name: String,
-        val url: String,
-        val color: String
+        @SerializedName("_class") var jenkinsClassName: String,
+        var name: String,
+        var url: String,
+        var color: String
 )
--- app/src/main/java/ru/pristalovpavel/personal/friendlyjenkins/repository/web/RetrofitSingleton.kt
+++ app/src/main/java/ru/pristalovpavel/personal/friendlyjenkins/repository/web/RetrofitSingleton.kt
@@ -1,2 +1,22 @@
 package ru.pristalovpavel.personal.friendlyjenkins.repository.web
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+enum class RetrofitSingleton{
+    INSTANCE;
+
+    val BASE_URL = "https://jenkins.altarix.ru"
+    var retrofit : Retrofit
+
+    fun init() {
+        if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+            }
+    }
+
+
+}
