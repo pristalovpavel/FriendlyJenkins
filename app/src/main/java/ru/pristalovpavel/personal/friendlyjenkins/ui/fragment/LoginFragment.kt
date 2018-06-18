4b7592f7d6dab1a9db581b0715cf4947f7de800a
Pavel Pristalov <pristalovpavel@gmail.com>
2018-06-18 19:04:34 +0300

    auth data store started

--- app/src/main/java/ru/pristalovpavel/personal/friendlyjenkins/ui/fragment/LoginFragment.kt
+++ app/src/main/java/ru/pristalovpavel/personal/friendlyjenkins/ui/fragment/LoginFragment.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.securepreferences.SecurePreferences
 import kotlinx.android.synthetic.main.fragment_login.*
 
 import ru.pristalovpavel.personal.friendlyjenkins.R
@@ -22,8 +24,60 @@ class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        continueLogin.setOnClickListener { attemptLogin() }
+    }
+
+    private fun attemptLogin() {
+        // Reset errors.
+        serverEditText.error = null
+        loginEditText.error = null
+        passwordEditText.error = null
+
+        // Store values at the time of the login attempt.
+        val serverString = serverEditText.text.toString()
+        val loginString = loginEditText.text.toString()
+        val passwordString = passwordEditText.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        if (TextUtils.isEmpty(serverString)) {
+            serverEditText.error = getString(R.string.error_server_required)
+            focusView = serverEditText
+            cancel = true
+        }
+
+        if (TextUtils.isEmpty(loginString)) {
+            loginEditText.error = getString(R.string.error_login_required)
+            if(focusView == null) focusView = loginEditText
+            cancel = true
+        }
+
+        if (TextUtils.isEmpty(passwordString) || !isPasswordValid(passwordString)) {
+            passwordEditText.error = getString(R.string.error_invalid_password)
+            if(focusView == null) focusView = passwordEditText
+            cancel = true
+        }
+
+        if (cancel) {
+            focusView?.requestFocus()
+        }
+        else {
+            if(!isAdded) return
+            val prefs = SecurePreferences(activity)
+            prefs.edit().putString("LOGIN", loginString)
+                    .putString("PASSWORD", passwordString)
+                    .commit()
+        }
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return password.length > 4
     }
 
     fun onButtonPressed(uri: Uri) {
